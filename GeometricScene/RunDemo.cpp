17359d546a17fd0c18ef87a622d6280beb628142
Andrey Mukhin <98459410+Alt0123@users.noreply.github.com>
2023-05-16 10:50:58 +0300

    Create RunDemo.cpp

---
+++ GeometricScene/RunDemo.cpp
@@ -0,0 +1,41 @@
+#include <sstream>
+
+#include "RunDemo.hpp"
+#include "SceneElement.hpp"
+#include "LengthSumVisitor.hpp"
+#include "GeometricScene.hpp"
+
+void RunDemo()
+{
+    Point p (10, 10, 0x808000);
+    Line l (Point(20, 10), Point(30, 50), 0x0000FF);
+    Polyline pl (std::vector<Point>{ Point(20, 10), Point(30, 50) }, 0x00FF00);
+    Ellipse el (Point(10, 20), 1, 1, 0x808000);
+    
+    GeometricScene s;
+    
+    s.AddItem(* new Point(p));
+    s.AddItem(* new Line(l));
+    s.AddItem(* new Polyline(pl));
+    s.AddItem(* new Ellipse(el));
+
+    LengthSumVisitor v;
+
+    for (s.First(); !s.IsDone(); s.Next())
+    {
+        s.Iterator()->Accept(v);
+    }
+    
+    std::cout << v.total_length << std::endl;
+    
+    std::stringstream ss;
+    s.Write(std::cout);
+    s.Write(ss);
+    std::cout << std::endl;
+    std::cout << ss.str() << std::endl;
+
+    GeometricScene s1;
+    s1.Read(ss);
+    s1.Write(std::cout);
+    std::cout << std::endl;
+}
