e839a4eb3d5d925168ba708acfdd641546555dd3
Andrey Mukhin <98459410+Alt0123@users.noreply.github.com>
2023-05-16 10:50:00 +0300

    Update main.cpp

--- GeometricScene/main.cpp
+++ GeometricScene/main.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <sstream>
+#include <dlfcn.h>
 
-#include "SceneElement.hpp"
-#include "LengthSumVisitor.hpp"
-#include "GeometricScene.hpp"
-
+typedef void (*PFN_SAY_NAME)();
 
 int main(int argc, char* argv[])
 {
-    Point p (10, 10, 0x808000);
-    Line l (Point(20, 10), Point(30, 50), 0x0000FF);
-    Polyline pl (std::vector<Point>{ Point(20, 10), Point(30, 50) }, 0x00FF00);
-    Ellipse el (Point(10, 20), 1, 1, 0x808000);
-    
-    GeometricScene s;
-    
-
-   s.AddItem(* new Point(p));
-   s.AddItem(* new Line(l));
-   s.AddItem(* new Polyline(pl));
-    s.AddItem(* new Ellipse(el));
-
-    LengthSumVisitor v;
-
-    for (s.First(); !s.IsDone(); s.Next())
-    {
-        s.Iterator()->Accept(v);
+    std::cout << "Result" << std::endl;
+    void* handle = dlopen("libGeom.dylib", RTLD_LAZY);
+    if (!handle) {
+        std::cout << "Could not open the library" << std::endl;
+        return 1;
     }
-    
-    std::cout << v.total_length << std::endl;
-    
-   std::stringstream ss;
-   s.Write(std::cout);
-   s.Write(ss);
-   std::cout << std::endl;
-   std::cout << ss.str() << std::endl;
 
-   GeometricScene s1;
-   s1.Read(ss);
-   s1.Write(std::cout);
-   std::cout << std::endl;
+    PFN_SAY_NAME RunDemoDyn = reinterpret_cast<PFN_SAY_NAME>(dlsym(handle, "RunDemo"));
+    if (!RunDemoDyn) {
+        std::cout << "Could not find RunDemo" << std::endl;
+        dlclose(handle);
+        return 1;
+    }
+
+    RunDemoDyn();
+    dlclose(handle);
     
     return 0;
 }
